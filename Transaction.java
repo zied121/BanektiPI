e2c9ad1723b48190a8834a04af4c7c9a1f70c942
ramma7121995 <156593195+ramma7121995@users.noreply.github.com>
2024-06-08 15:23:22 +0100

    Add files via upload
    entite transaction

---
+++ Transaction.java
@@ -0,0 +1,56 @@
+package entite;
+
+public class Transaction {
+    private int id;
+    private String localdate;
+    private String type;
+    private double montant;
+    private String statut;
+
+
+
+    private User id_user;
+
+    public Transaction(int i, String text, String text1, double v, String text2) {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getMontant() {
+        return montant;
+    }
+
+    public void setMontant(double montant) {
+        this.montant = montant;
+    }
+
+    public String getStatut() {
+        return statut;
+    }
+
+    public void setStatut(String statut) {
+        this.statut = statut;
+    }
+}
